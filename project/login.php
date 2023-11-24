00ab4c00c0cb22adb5cc0555635d20aad500846d
jeremy0919 <jeremy0919@icloud.com>
2023-11-24 15:40:47 -0800

    update

--- project/login.php
+++ project/login.php
@@ -1,21 +1,14 @@
-<?php // start at log in page, log in page goes to selection of one or two player // start at log in page, log in page goes to selection of one or two player
-        // Read the existing JSON file
-        include("databaseT.php");  
+<?php
+include("databaseT.php");
+
 if (isset($_POST['submitL'])) {
-        $name = trim($_POST['username']);
-        $password = trim($_POST['password']);
-
-
+    $name = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
     $stmt = $connection->prepare("SELECT * FROM leaderboards WHERE name = ?");
-
-    // Bind parameters
-    $stmt->bind_param("s",  $name);
-
-    // Execute query
+    $stmt->bind_param("s", $name);
     $stmt->execute();
 
-    // Get the result
     $result = $stmt->get_result();
     $data = array();
 
@@ -26,17 +19,26 @@ if (isset($_POST['submitL'])) {
         while ($row = $result->fetch_assoc()) {
             $data[] = $row;
         }
-    }
-    if($data["password"] == $password){
-        echo json_encode($data);
-        header('Location: game.html');
-    }
-    else{
-        echo json_encode(array("msg"=> "Wrong password")); // idk if this is right
+
+        // Verify the entered password against the hashed password in the database
+        if (password_verify($password, $data[0]['password'])) {
+            echo json_encode($data);
+            $connection->close();
+            header('Location: game.html');
+            exit;
+        } else {
+            echo json_encode(array("msg" => "Wrong password"));
+            $connection->close();
+            header('Location: login.html');
+            exit;
+        }
+    } else {
+        echo json_encode(array("msg" => "Account not found"));
+        $connection->close();
         header('Location: login.html');
+        exit;
     }
 
-    $connection->close();
-}         
-            
+   
+}
 ?>
--- project/newUser.php
+++ project/newUser.php
@@ -2,28 +2,32 @@
 
 include("databaseT.php");
 
-    
-if( isset($_POST['Sname'] )&& isset($_POST['password'])){ // somethings buggy
-   $name = trim($_POST['Sname']);
-   $password = trim($_POST['password']);
+if (isset($_POST['Sname']) && isset($_POST['password'])) {
+    $name = trim($_POST['Sname']);
+    $password = trim($_POST['password']);
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
     $stmt = $connection->prepare("SELECT * FROM leaderboards WHERE name = ?");
     $stmt->bind_param("s", $name);
     $stmt->execute();
-    
+
     $result = $stmt->get_result();
-    
+
     if ($result->num_rows > 0) {
-        Echo("account already exits");
+        echo "Account already exists";
     } else {
         // Name not found in the database, do something else
-            $sql = "INSERT INTO `leaderboards` (`id`, `name`, `password`, `gamesPlayed`, `wins`, `losses`, `winRate`) 
-            VALUES (NULL, '$name','$password','0','0','0','0')";
-            mysqli_query($connection,$sql);
-        
-        }
-    
+        $sql = "INSERT INTO `leaderboards` (`id`, `name`, `password`, `gamesPlayed`, `wins`, `losses`, `winRate`) 
+                VALUES (NULL, ?, ?, '0', '0', '0', '0')";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("ss", $name, $hashed_password);
+        $stmt->execute();
+    }
+
     $stmt->close();
     $connection->close();
-    }
-    header('Location: checkers.html');
+}
+
+header('Location: checkers.html');
+exit;
 ?>
